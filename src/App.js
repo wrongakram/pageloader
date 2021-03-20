021d05c3be5a208cfa582174afeef95d9e1798ef
Akram Khalid <akramkhalid12@gmail.com>
2021-03-20 14:30:43 -0500

    test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>HI</p>
+        <p>Hello akram</p>
       </header>
     </div>
   );
