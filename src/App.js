b37981c17f06c41fe1ce25f2f8ad8840fec30a14
Akram Khalid <akramkhalid12@gmail.com>
2021-03-20 13:44:38 -0500

    init

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>Hello</p>
+        <p>HI</p>
       </header>
     </div>
   );
